1e4b20be84e9784c1e6cd79d7020fb86b1526914
Koliadjun <koliadjun@gmail.com>
2021-09-10 02:12:38 +0300

    clean unused files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import './App.css';
 import React from 'react';
 
-import Phonebook from './components/Phonebook';
-import data from './components/data.json';
-
 const App = () => {
-  return <Phonebook contacts={data} />;
+  <></>;
 };
 
 export default App;
