efc71417261b670c23d6e0ec7685f71aee23811f
Koliadjun <koliadjun@gmail.com>
2021-09-14 01:51:39 +0300

    API first implementation

---
+++ src/services/API/api_tmdb.js
@@ -0,0 +1,13 @@
+const API_KEY = '05b27f765345223aac972c2dbb5eec37';
+const BASE_URL = 'https://api.themoviedb.org/3/';
+async function fetchWithErrorHandling(url = '', config = {}) {
+  const response = await fetch(url, config);
+  return response.ok
+    ? await response.json()
+    : Promise.reject(new Error('Some thing went wrong!'));
+}
+export function getTrendingMovie() {
+  return fetchWithErrorHandling(
+    `${BASE_URL}//trending/movie/day?api_key=${API_KEY}`,
+  );
+}
