cb84d188e8ab2a9287d89ff448d3e497352bdfa8
Koliadjun <koliadjun@gmail.com>
2021-09-14 01:51:51 +0300

    Added route

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
 import './App.css';
 import React from 'react';
+import { Route, Switch } from 'react-router';
+import HomePage from './views/HomePage/HomePage';
+import Movies from './views/Movies/Movies';
+import Navigation from './components/Navigation/Navigation';
+import NotFondPage from './views/NotFoundPage/NotFondPage';
 
 const App = () => {
-  return <></>;
+  return (
+    <>
+      <Navigation />
+      <Switch>
+        <Route exact path="/">
+          <HomePage />
+        </Route>
+        <Route exact path="/movies">
+          <Movies />
+        </Route>
+        <Route path="/movies/:movieId"></Route>
+        <Route path="/movies/:movieId/cast"></Route>
+        <Route path="/movies/:movieId/reviews"></Route>
+        <Route>
+          <NotFondPage />
+        </Route>
+      </Switch>
+    </>
+  );
 };
 
 export default App;
